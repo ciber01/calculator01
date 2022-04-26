7073321357735971321c2713c6126fe2e14136d5
Ciber 01 <29ciber01@gmail.com>
2022-04-26 19:38:10 +0200

    jaja

--- src/main/java/com/ciber01/calculator01/Calculator01Application.java
+++ src/main/java/com/ciber01/calculator01/Calculator01Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Calculator01Application.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(Calculator01Application.class, args);
+}
 
 }
